8a7b33296806244c8e93bd2e81b934e72f0806da
Mario Vrevic <80117929+MariooV@users.noreply.github.com>
2023-09-06 18:33:52 +0200

    task 1

---
+++ src/components/AppCars.js
@@ -0,0 +1,5 @@
+const AppCars = () => {
+  return <h1>CARS BRM BRM </h1>;
+};
+
+export default AppCars;
---
+++ src/pages/Cars.js
@@ -0,0 +1,13 @@
+import AppCars from "../components/AppCars";
+import Header from "../components/Header";
+
+const Cars = () => {
+  return (
+    <>
+      <Header />
+      <AppCars />
+    </>
+  );
+};
+
+export default Cars;
